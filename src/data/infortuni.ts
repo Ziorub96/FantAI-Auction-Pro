5e76aa2355b4a3f0614cd3609dfe3eb01f8570c6
Ziorub96 <168661787+Ziorub96@users.noreply.github.com>
2026-09-03 08:58:12 +0200

    Create infortuni.ts

---
+++ src/data/infortuni.ts
@@ -0,0 +1,453 @@
+export const INFORTUNI = [
+  {
+    "giocatore": "Evan Ndicka",
+    "ruolo": "Difensore centrale",
+    "squadra": "Roma",
+    "infortunio": "Problema muscolare",
+    "dal": "31/08/2026",
+    "fino_ca": null,
+    "valore_mercato": "35,00 mln €"
+  },
+  {
+    "giocatore": "Romano Schmid",
+    "ruolo": "Trequartista",
+    "squadra": null,
+    "infortunio": "Affaticamento muscolare",
+    "dal": "31/08/2026",
+    "fino_ca": null,
+    "valore_mercato": "15,00 mln €"
+  },
+  {
+    "giocatore": "Daniel Boloca",
+    "ruolo": "Mediano",
+    "squadra": null,
+    "infortunio": "Artroscopia",
+    "dal": "18/02/2026",
+    "fino_ca": null,
+    "valore_mercato": "4,00 mln €"
+  },
+  {
+    "giocatore": "Gustavo Varela",
+    "ruolo": "Punta centrale",
+    "squadra": "Monza",
+    "infortunio": "Problema agli adduttori",
+    "dal": "31/08/2026",
+    "fino_ca": null,
+    "valore_mercato": "3,00 mln €"
+  },
+  {
+    "giocatore": "Patrick Ciurria",
+    "ruolo": "Esterno di destra",
+    "squadra": null,
+    "infortunio": "Problema fisico",
+    "dal": "14/08/2026",
+    "fino_ca": null,
+    "valore_mercato": "1,00 mln €"
+  },
+  {
+    "giocatore": "Yael Trepy",
+    "ruolo": "Punta centrale",
+    "squadra": null,
+    "infortunio": "Problema fisico",
+    "dal": "17/08/2026",
+    "fino_ca": null,
+    "valore_mercato": "2,00 mln €"
+  },
+  {
+    "giocatore": "Yerry Mina",
+    "ruolo": "Difensore centrale",
+    "squadra": "Cagliari",
+    "infortunio": "Problema al polpaccio",
+    "dal": "29/08/2026",
+    "fino_ca": null,
+    "valore_mercato": "2,50 mln €"
+  },
+  {
+    "giocatore": "Willem Geubbels",
+    "ruolo": "Punta centrale",
+    "squadra": "Lecce",
+    "infortunio": "Distorsione alla caviglia",
+    "dal": "01/09/2026",
+    "fino_ca": null,
+    "valore_mercato": "5,00 mln €"
+  },
+  {
+    "giocatore": "Thomas Kristensen",
+    "ruolo": "Difensore centrale",
+    "squadra": "Atalanta",
+    "infortunio": "Problemi alla caviglia",
+    "dal": "22/08/2026",
+    "fino_ca": null,
+    "valore_mercato": "12,00 mln €"
+  },
+  {
+    "giocatore": "Andrea Cambiaso",
+    "ruolo": "Terzino sinistro",
+    "squadra": "Juventus",
+    "infortunio": "Distorsione alla caviglia",
+    "dal": "30/08/2026",
+    "fino_ca": null,
+    "valore_mercato": "20,00 mln €"
+  },
+  {
+    "giocatore": "Filipe Bordon",
+    "ruolo": "Difensore centrale",
+    "squadra": null,
+    "infortunio": "Frattura del metatarso",
+    "dal": "23/07/2026",
+    "fino_ca": null,
+    "valore_mercato": "750 mila €"
+  },
+  {
+    "giocatore": "Alessio Furlanetto",
+    "ruolo": "Portiere",
+    "squadra": null,
+    "infortunio": "Operazione al legamento crociato",
+    "dal": "01/07/2026",
+    "fino_ca": null,
+    "valore_mercato": "350 mila €"
+  },
+  {
+    "giocatore": "Matteo Gabbia",
+    "ruolo": "Difensore centrale",
+    "squadra": "Milan",
+    "infortunio": "Problemi alla caviglia",
+    "dal": "01/09/2026",
+    "fino_ca": null,
+    "valore_mercato": "17,00 mln €"
+  },
+  {
+    "giocatore": "Luca Pellegrini",
+    "ruolo": "Terzino sinistro",
+    "squadra": "Roma",
+    "infortunio": "Problemi alla coscia",
+    "dal": "20/08/2026",
+    "fino_ca": null,
+    "valore_mercato": "4,50 mln €"
+  },
+  {
+    "giocatore": "Devyne Rensch",
+    "ruolo": "Terzino destro",
+    "squadra": "Roma",
+    "infortunio": "Problema muscolare",
+    "dal": "23/08/2026",
+    "fino_ca": null,
+    "valore_mercato": "8,50 mln €"
+  },
+  {
+    "giocatore": "Hamed Traoré",
+    "ruolo": "Ala sinistra",
+    "squadra": null,
+    "infortunio": "Problema agli adduttori",
+    "dal": "02/05/2026",
+    "fino_ca": null,
+    "valore_mercato": "15,00 mln €"
+  },
+  {
+    "giocatore": "Patric",
+    "ruolo": "Difensore centrale",
+    "squadra": "Lazio",
+    "infortunio": "Stiramento muscolare",
+    "dal": "14/07/2026",
+    "fino_ca": null,
+    "valore_mercato": "1,50 mln €"
+  },
+  {
+    "giocatore": "Danilo Cataldi",
+    "ruolo": "Mediano",
+    "squadra": "Lazio",
+    "infortunio": "Infiammazione all'osso pubico",
+    "dal": "19/05/2026",
+    "fino_ca": null,
+    "valore_mercato": "3,50 mln €"
+  },
+  {
+    "giocatore": "Weston McKennie",
+    "ruolo": "Centrocampista",
+    "squadra": "Juventus",
+    "infortunio": "Affaticamento muscolare",
+    "dal": "27/08/2026",
+    "fino_ca": "04/09/2026",
+    "valore_mercato": "30,00 mln €"
+  },
+  {
+    "giocatore": "Lorenzo Venturino",
+    "ruolo": "Ala destra",
+    "squadra": null,
+    "infortunio": "Operazione al ginocchio",
+    "dal": "31/05/2026",
+    "fino_ca": "04/09/2026",
+    "valore_mercato": "5,00 mln €"
+  },
+  {
+    "giocatore": "Matías Moreno",
+    "ruolo": "Difensore centrale",
+    "squadra": null,
+    "infortunio": "Problema muscolare",
+    "dal": "18/08/2026",
+    "fino_ca": "04/09/2026",
+    "valore_mercato": "6,00 mln €"
+  },
+  {
+    "giocatore": "Pietro Pellegri",
+    "ruolo": "Punta centrale",
+    "squadra": null,
+    "infortunio": "Rottura del legamento crociato",
+    "dal": "16/01/2026",
+    "fino_ca": "05/09/2026",
+    "valore_mercato": "500 mila €"
+  },
+  {
+    "giocatore": "Adrián Bernabé",
+    "ruolo": "Centrocampista",
+    "squadra": "Parma",
+    "infortunio": "Affaticamento muscolare",
+    "dal": "31/08/2026",
+    "fino_ca": "05/09/2026",
+    "valore_mercato": "15,00 mln €"
+  },
+  {
+    "giocatore": "Hans Nicolussi Caviglia",
+    "ruolo": "Mediano",
+    "squadra": null,
+    "infortunio": "Pubalgia",
+    "dal": "23/07/2026",
+    "fino_ca": "07/09/2026",
+    "valore_mercato": "6,00 mln €"
+  },
+  {
+    "giocatore": "Fali Candé",
+    "ruolo": "Difensore centrale",
+    "squadra": null,
+    "infortunio": "Rottura del legamento crociato",
+    "dal": "14/01/2026",
+    "fino_ca": "15/09/2026",
+    "valore_mercato": "3,00 mln €"
+  },
+  {
+    "giocatore": "Matteo Palma",
+    "ruolo": "Difensore centrale",
+    "squadra": "Udinese",
+    "infortunio": "Infortunio muscolare",
+    "dal": "23/08/2026",
+    "fino_ca": "15/09/2026",
+    "valore_mercato": "6,50 mln €"
+  },
+  {
+    "giocatore": "Adam Marusic",
+    "ruolo": "Terzino destro",
+    "squadra": "Lazio",
+    "infortunio": "Infortunio alla coscia",
+    "dal": "24/08/2026",
+    "fino_ca": "17/09/2026",
+    "valore_mercato": "2,50 mln €"
+  },
+  {
+    "giocatore": "Nicolò Rovella",
+    "ruolo": "Mediano",
+    "squadra": "Lazio",
+    "infortunio": "Infortunio al polpaccio",
+    "dal": "31/08/2026",
+    "fino_ca": "18/09/2026",
+    "valore_mercato": "23,00 mln €"
+  },
+  {
+    "giocatore": "Fisayo Dele-Bashiru",
+    "ruolo": "Centrocampista",
+    "squadra": null,
+    "infortunio": "Infortunio alla coscia",
+    "dal": "24/08/2026",
+    "fino_ca": "18/09/2026",
+    "valore_mercato": "6,00 mln €"
+  },
+  {
+    "giocatore": "Oussama El Azzouzi",
+    "ruolo": "Mediano",
+    "squadra": null,
+    "infortunio": "Infortunio al bicipite femorale",
+    "dal": "25/08/2026",
+    "fino_ca": "22/09/2026",
+    "valore_mercato": "1,80 mln €"
+  },
+  {
+    "giocatore": "Riccardo Orsolini",
+    "ruolo": "Ala destra",
+    "squadra": "Bologna",
+    "infortunio": "Infortunio alla coscia",
+    "dal": "31/08/2026",
+    "fino_ca": "22/09/2026",
+    "valore_mercato": "15,00 mln €"
+  },
+  {
+    "giocatore": "Isak Hien",
+    "ruolo": "Difensore centrale",
+    "squadra": "Atalanta",
+    "infortunio": "Problemi alla coscia",
+    "dal": "30/06/2026",
+    "fino_ca": "30/09/2026",
+    "valore_mercato": "22,00 mln €",
+    "nota": "Operato; rientro stimato metà ottobre-inizio novembre secondo Fantamaster"
+  },
+  {
+    "giocatore": "Kamaldeen Sulemana",
+    "ruolo": "Ala sinistra",
+    "squadra": null,
+    "infortunio": "Infortunio al legamento collaterale mediale",
+    "dal": "09/08/2026",
+    "fino_ca": "01/10/2026",
+    "valore_mercato": "17,00 mln €"
+  },
+  {
+    "giocatore": "Andrea Adorante",
+    "ruolo": "Punta centrale",
+    "squadra": null,
+    "infortunio": "Infortunio alla schiena",
+    "dal": "25/07/2026",
+    "fino_ca": "01/10/2026",
+    "valore_mercato": "4,50 mln €"
+  },
+  {
+    "giocatore": "Edoardo Pieragnolo",
+    "ruolo": "Terzino sinistro",
+    "squadra": null,
+    "infortunio": "Infortunio al legamento crociato",
+    "dal": "01/10/2025",
+    "fino_ca": "01/10/2026",
+    "valore_mercato": "2,00 mln €"
+  },
+  {
+    "giocatore": "Nicolò Zaniolo",
+    "ruolo": "Seconda punta",
+    "squadra": "Udinese",
+    "infortunio": "Stiramento alla coscia",
+    "dal": "23/08/2026",
+    "fino_ca": "01/10/2026",
+    "valore_mercato": "15,00 mln €"
+  },
+  {
+    "giocatore": "Alessandro Zanoli",
+    "ruolo": "Terzino destro",
+    "squadra": null,
+    "infortunio": "Rottura del legamento crociato",
+    "dal": "28/01/2026",
+    "fino_ca": "01/10/2026",
+    "valore_mercato": "4,50 mln €"
+  },
+  {
+    "giocatore": "Scott McTominay",
+    "ruolo": "Centrocampista",
+    "squadra": "Napoli",
+    "infortunio": "Problemi con il cuore",
+    "dal": "01/09/2026",
+    "fino_ca": "01/10/2026",
+    "valore_mercato": "40,00 mln €"
+  },
+  {
+    "giocatore": "Jayden Addai",
+    "ruolo": "Ala destra",
+    "squadra": null,
+    "infortunio": "Lacerazione del tendine d'achille",
+    "dal": "26/02/2026",
+    "fino_ca": "01/10/2026",
+    "valore_mercato": "20,00 mln €"
+  },
+  {
+    "giocatore": "Luca Marianucci",
+    "ruolo": "Difensore centrale",
+    "squadra": null,
+    "infortunio": "Infortunio al legamento collaterale mediale",
+    "dal": "10/08/2026",
+    "fino_ca": "12/10/2026",
+    "valore_mercato": "7,00 mln €"
+  },
+  {
+    "giocatore": "Fabiano Parisi",
+    "ruolo": "Terzino sinistro",
+    "squadra": null,
+    "infortunio": "Infortunio al legamento crociato",
+    "dal": "18/05/2026",
+    "fino_ca": "19/10/2026",
+    "valore_mercato": "6,00 mln €"
+  },
+  {
+    "giocatore": "Riyad Idrissi",
+    "ruolo": "Terzino sinistro",
+    "squadra": null,
+    "infortunio": "Rottura del legamento crociato",
+    "dal": "08/03/2026",
+    "fino_ca": "30/10/2026",
+    "valore_mercato": "4,00 mln €"
+  },
+  {
+    "giocatore": "Alessandro Buongiorno",
+    "ruolo": "Difensore centrale",
+    "squadra": "Napoli",
+    "infortunio": "Operazione al ginocchio",
+    "dal": "17/07/2026",
+    "fino_ca": "31/10/2026",
+    "valore_mercato": "30,00 mln €"
+  },
+  {
+    "giocatore": "Marin Šverko",
+    "ruolo": "Difensore centrale",
+    "squadra": null,
+    "infortunio": "Problema fisico",
+    "dal": "01/07/2026",
+    "fino_ca": "31/10/2026",
+    "valore_mercato": "1,50 mln €"
+  },
+  {
+    "giocatore": "Matteo Pessina",
+    "ruolo": "Centrocampista",
+    "squadra": "Monza",
+    "infortunio": "Infortunio al ginocchio",
+    "dal": "13/08/2026",
+    "fino_ca": "01/11/2026",
+    "valore_mercato": "5,00 mln €"
+  },
+  {
+    "giocatore": "Jeff Ekhator",
+    "ruolo": "Punta centrale",
+    "squadra": null,
+    "infortunio": "Stiramento alla coscia",
+    "dal": "30/08/2026",
+    "fino_ca": "02/11/2026",
+    "valore_mercato": "18,00 mln €"
+  },
+  {
+    "giocatore": "Kenan Yıldız",
+    "ruolo": "Ala sinistra",
+    "squadra": "Juventus",
+    "infortunio": "Frattura del piede",
+    "dal": "24/08/2026",
+    "fino_ca": "25/11/2026",
+    "valore_mercato": "75,00 mln €"
+  },
+  {
+    "giocatore": "Franco Israel",
+    "ruolo": "Portiere",
+    "squadra": null,
+    "infortunio": "Infortunio alla spalla",
+    "dal": "19/06/2026",
+    "fino_ca": "04/12/2026",
+    "valore_mercato": "2,50 mln €"
+  },
+  {
+    "giocatore": "Khéphren Thuram",
+    "ruolo": "Centrocampista",
+    "squadra": "Juventus",
+    "infortunio": "Problemi al ginocchio",
+    "dal": "13/07/2026",
+    "fino_ca": "01/01/2027",
+    "valore_mercato": "38,00 mln €"
+  },
+  {
+    "giocatore": "Ismaël Koné",
+    "ruolo": "Centrocampista",
+    "squadra": "Roma",
+    "infortunio": "Frattura di tibia e perone",
+    "dal": "19/06/2026",
+    "fino_ca": "03/01/2027",
+    "valore_mercato": "25,00 mln €"
+  }
+];
