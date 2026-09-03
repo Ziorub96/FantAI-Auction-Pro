5b616ef658dbebc0cdb9c6a322a21d7d30bfc296
Ziorub96 <168661787+Ziorub96@users.noreply.github.com>
2026-09-03 10:18:23 +0200

    Create normalizza.ts

---
+++ src/lib/normalizza.ts
@@ -0,0 +1,8 @@
+export function normalizzaNome(nome: string): string {
+  return nome
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/\s+/g, " ")
+    .trim();
+}
