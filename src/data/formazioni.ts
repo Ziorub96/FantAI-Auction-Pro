52c85fad01107abdc7ae555a4c6061f103272702
Ziorub96 <168661787+Ziorub96@users.noreply.github.com>
2026-09-03 08:55:25 +0200

    Create formazioni.ts

---
+++ src/data/formazioni.ts
@@ -0,0 +1,248 @@
+export const FORMAZIONI = [
+  {
+    "squadra": "Atalanta",
+    "modulo": "4-3-3",
+    "allenatore": "Sarri",
+    "formazione": "Carnesecchi; Zappacosta/Bellanova, Kristensen/Kossounou, Scalvini/Hien, Bernasconi/Kolasinac; Ederson, Gaetano, Kessie/Pasalic/Samardzic; De Ketelaere, Scamacca/Krstovic, Raspadori/Rowe",
+    "ballottaggi": [
+      {"posizione": "TD", "giocatori": ["Zappacosta", "Bellanova"], "percentuali": [75, 25]},
+      {"posizione": "DC", "giocatori": ["Kristensen", "Kossounou"], "percentuali": [50, 50]},
+      {"posizione": "DC", "giocatori": ["Scalvini", "Hien"], "percentuali": [65, 35], "nota": "ballottaggio aperto non appena Hien tornerà dall'infortunio a metà ottobre-inizio novembre"},
+      {"posizione": "TS", "giocatori": ["Bernasconi", "Kolasinac"], "percentuali": [50, 50]},
+      {"posizione": "CC", "giocatori": ["Kessie", "Pasalic", "Samardzic"], "percentuali": [55, 25, 20]},
+      {"posizione": "ATT", "giocatori": ["Scamacca", "Krstovic"], "percentuali": [60, 40]},
+      {"posizione": "ATT", "giocatori": ["Raspadori", "Rowe"], "percentuali": [60, 40]}
+    ]
+  },
+  {
+    "squadra": "Bologna",
+    "modulo": "4-3-3",
+    "allenatore": "Tedesco",
+    "formazione": "Skorupski; Zortea/Holm, Theate, Heggem/Helland, Miranda/Alhassane; Ferguson, Amondarain/Pobega/Moro, Bernardeschi/Odgaard; Orsolini, Piccoli/Dovbyk, Cambiaghi/Mbangula",
+    "ballottaggi": [
+      {"posizione": "TD", "giocatori": ["Zortea", "Holm"], "percentuali": [55, 45]},
+      {"posizione": "DC", "giocatori": ["Heggem", "Helland"], "percentuali": [65, 35]},
+      {"posizione": "TS", "giocatori": ["Miranda", "Alhassane"], "percentuali": [50, 50]},
+      {"posizione": "CC", "giocatori": ["Amondarain", "Pobega", "Moro"], "percentuali": [40, 30, 30], "nota": "Moro potrebbe scalare le gerarchie non appena avrà recuperato la migliore condizione fisica"},
+      {"posizione": "ATT", "giocatori": ["Bernardeschi", "Odgaard"], "percentuali": [55, 45]},
+      {"posizione": "ATT", "giocatori": ["Piccoli", "Dovbyk"], "percentuali": [55, 45]},
+      {"posizione": "ATT", "giocatori": ["Cambiaghi", "Mbangula"], "percentuali": [55, 45]}
+    ]
+  },
+  {
+    "squadra": "Cagliari",
+    "modulo": "4-3-2-1",
+    "allenatore": "Pisacane",
+    "formazione": "Caprile; Sugawara/Ze Pedro, Mina, Rodriguez, Obert; Adopo, Winks, Romano, Fazzini/Fadera, Maldini; Kevin Carlos/Nzola/Mendy",
+    "ballottaggi": [
+      {"posizione": "TD", "giocatori": ["Sugawara", "Ze Pedro"], "percentuali": [70, 30]},
+      {"posizione": "CC", "giocatori": ["Fazzini", "Fadera"], "percentuali": [55, 45]},
+      {"posizione": "ATT", "giocatori": ["Kevin Carlos", "Nzola", "Mendy"], "percentuali": [40, 35, 25]}
+    ]
+  },
+  {
+    "squadra": "Como",
+    "modulo": "4-2-3-1",
+    "allenatore": "Fabregas",
+    "formazione": "Butez; Couto, Ramon, Chalobah, Valle/Kaiki; Milla/Da Cunha, Da Cunha/Perrone; Diao, Nico Paz, Baturina/J.Rodriguez; Kean/Douvikas",
+    "ballottaggi": [
+      {"posizione": "TS", "giocatori": ["Valle", "Kaiki"], "percentuali": [51, 49]},
+      {"posizione": "CC", "giocatori": ["Milla", "Da Cunha"], "percentuali": [55, 45]},
+      {"posizione": "CC", "giocatori": ["Da Cunha", "Perrone"], "percentuali": [50, 50]},
+      {"posizione": "ATT", "giocatori": ["Baturina", "J. Rodriguez"], "percentuali": [70, 30]},
+      {"posizione": "ATT", "giocatori": ["Kean", "Douvikas"], "percentuali": [55, 45]}
+    ]
+  },
+  {
+    "squadra": "Fiorentina",
+    "modulo": "4-3-2-1",
+    "allenatore": "Grosso",
+    "formazione": "De Gea; Jimenez/Dodò, Dragusin, Pongracic/Viery/Ranieri, Valdepenas; Atta, Fagioli/Oulai, Ndour; Mastantuono, Goncalves/Njie; Pellegrino/Beto",
+    "ballottaggi": [
+      {"posizione": "TD", "giocatori": ["Jimenez", "Dodò"], "percentuali": [60, 40]},
+      {"posizione": "DC", "giocatori": ["Pongracic", "Viery", "Ranieri"], "percentuali": [55, 35, 10]},
+      {"posizione": "CC", "giocatori": ["Fagioli", "Oulai"], "percentuali": [50, 50]},
+      {"posizione": "ATT", "giocatori": ["Pellegrino", "Beto"], "percentuali": [60, 40]},
+      {"posizione": "ATT", "giocatori": ["Goncalves", "Njie"], "percentuali": [60, 40]}
+    ]
+  },
+  {
+    "squadra": "Frosinone",
+    "modulo": "4-2-3-1",
+    "allenatore": "Alvini",
+    "formazione": "Palmisani; Oyono/Tchato, Calvani/Akpoguma, Monterisi, Bracaglia/Terzic; Calò/Grillitsch, Masini/Chichella; Fini/Zerbin, Schmid; Raimondo, Kvernadze",
+    "ballottaggi": [
+      {"posizione": "TD", "giocatori": ["Oyono", "Tchato"], "percentuali": [51, 49]},
+      {"posizione": "DC", "giocatori": ["Calvani", "Akpoguma"], "percentuali": [55, 45]},
+      {"posizione": "TS", "giocatori": ["Bracaglia", "Terzic"], "percentuali": [55, 45]},
+      {"posizione": "CC", "giocatori": ["Calò", "Grillitsch"], "percentuali": [70, 30]},
+      {"posizione": "CC", "giocatori": ["Masini", "Chichella"], "percentuali": [65, 35]},
+      {"posizione": "ATT", "giocatori": ["Fini", "Zerbin"], "percentuali": [55, 45]}
+    ]
+  },
+  {
+    "squadra": "Genoa",
+    "modulo": "3-4-2-1",
+    "allenatore": "De Rossi",
+    "formazione": "Bijlow; Marcandalli, Ostigard, Vasquez; Drameh/Ellertsson, Frendrup, Sow/Amorim, Ellertsson/Mitaj; Baldanzi/Traorè, Vitinha/El Shaarawy/Meichtry; Colombo/Osmajic",
+    "ballottaggi": [
+      {"posizione": "CC", "giocatori": ["Sow", "Amorim"], "percentuali": [60, 40]},
+      {"posizione": "TD", "giocatori": ["Drameh", "Ellertsson"], "percentuali": [55, 45]},
+      {"posizione": "TS", "giocatori": ["Ellertsson", "Mitaj"], "percentuali": [50, 50]},
+      {"posizione": "ATT", "giocatori": ["Baldanzi", "Traorè"], "percentuali": [55, 45]},
+      {"posizione": "ATT", "giocatori": ["Vitinha", "El Shaarawy", "Meichtry"], "percentuali": [45, 35, 20]},
+      {"posizione": "ATT", "giocatori": ["Colombo", "Osmajic"], "percentuali": [70, 30]}
+    ]
+  },
+  {
+    "squadra": "Inter",
+    "modulo": "3-5-2",
+    "allenatore": null,
+    "formazione": "Martinez; Bisseck/Stones, Akanji, Bastoni; Spence/Diouf/Luis Henrique, Barella, Calhanoglu, Zielinski/Sucic, Dimarco; Lautaro Martinez, Thuram/Pio Esposito",
+    "ballottaggi": [
+      {"posizione": "DC", "giocatori": ["Bisseck", "Stones"], "percentuali": [50, 50]},
+      {"posizione": "TD", "giocatori": ["Spence", "Diouf", "Luis Henrique"], "percentuali": [45, 35, 30]},
+      {"posizione": "CC", "giocatori": ["Zielinski", "Sucic"], "percentuali": [65, 35]},
+      {"posizione": "ATT", "giocatori": ["Thuram", "Pio Esposito"], "percentuali": [65, 35]}
+    ]
+  },
+  {
+    "squadra": "Juventus",
+    "modulo": "4-2-3-1",
+    "allenatore": "Spalletti",
+    "formazione": "Vicario; Kalulu, Bremer, Lucumì/Kelly, Celik/Cambiaso; Locatelli, Douglas Luiz/Sarr; Conceicao, McKennie, Boga/Alajbegovic; Kolo Muani/Woltemade",
+    "ballottaggi": [
+      {"posizione": "DC", "giocatori": ["Lucumì", "Kelly"], "percentuali": [70, 30]},
+      {"posizione": "TS", "giocatori": ["Celik", "Cambiaso"], "percentuali": [55, 45]},
+      {"posizione": "CC", "giocatori": ["Douglas Luiz", "Sarr"], "percentuali": [60, 40]},
+      {"posizione": "ATT", "giocatori": ["Boga", "Alajbegovic"], "percentuali": [55, 45]},
+      {"posizione": "ATT", "giocatori": ["Kolo Muani", "Woltemade"], "percentuali": [60, 40]}
+    ]
+  },
+  {
+    "squadra": "Lazio",
+    "modulo": "4-3-3",
+    "allenatore": "Gattuso",
+    "formazione": "Mandas; Marusic, Sutalo/Doekhi, Provstgaard, Nuno Tavares/Pedraza; Frattesi, Rovella, Taylor; Gudmundsson, Pinamonti, Zaccagni",
+    "ballottaggi": [
+      {"posizione": "DC", "giocatori": ["Sutalo", "Doekhi"], "percentuali": [55, 45]},
+      {"posizione": "TS", "giocatori": ["Nuno Tavares", "Pedraza"], "percentuali": [70, 30]}
+    ]
+  },
+  {
+    "squadra": "Lecce",
+    "modulo": "4-2-3-1",
+    "allenatore": "Di Francesco",
+    "formazione": "Falcone; D. Veiga, Gaspar/Siebert, Tiago Gabriel, Gallo; Ilic, Coulibaly/Berisha; Pierotti/Monteiro, Gorter/Gandelman, N’dri/Fatah; Stulic/Geubbels",
+    "ballottaggi": [
+      {"posizione": "DC", "giocatori": ["Gaspar", "Siebert"], "percentuali": [60, 40]},
+      {"posizione": "CC", "giocatori": ["Coulibaly", "Berisha"], "percentuali": [55, 45]},
+      {"posizione": "ATT", "giocatori": ["Pierotti", "Monteiro"], "percentuali": [60, 40]},
+      {"posizione": "ATT", "giocatori": ["Gorter", "Gandelman"], "percentuali": [55, 45]},
+      {"posizione": "ATT", "giocatori": ["N’dri", "Fatah"], "percentuali": [55, 45]},
+      {"posizione": "ATT", "giocatori": ["Stulic", "Geubbels"], "percentuali": [50, 50]}
+    ]
+  },
+  {
+    "squadra": "Milan",
+    "modulo": "3-4-2-1",
+    "allenatore": "Amorim",
+    "formazione": "Maignan; Gila, Gabbia, Pavlovic; Chukwueze/Moreira, Modric/Jashari, Rabiot, Bartesaghi/Moreira; Cissè/Loftus Cheek/Hutchinson, Pulisic; Goncalo Ramos",
+    "ballottaggi": [
+      {"posizione": "TD", "giocatori": ["Chukwueze", "Moreira"], "percentuali": [60, 40]},
+      {"posizione": "TS", "giocatori": ["Bartesaghi", "Moreira"], "percentuali": [60, 40]},
+      {"posizione": "CC", "giocatori": ["Modric", "Jashari"], "percentuali": [65, 35]},
+      {"posizione": "ATT", "giocatori": ["Cissè", "Loftus Cheek", "Hutchinson"], "percentuali": [50, 30, 20]}
+    ]
+  },
+  {
+    "squadra": "Monza",
+    "modulo": "3-4-2-1",
+    "allenatore": "Juric",
+    "formazione": "Tornqvist; Kouadio/Goglichidze, Ziolkowski/Lucchesi, Carboni; Birindelli, Folorunsho/Foe Ondoa, Mout/Akinsamiro, Mangas; Colpani/Zeballos, Ngonge/Cutrone; Varela",
+    "ballottaggi": [
+      {"posizione": "DC", "giocatori": ["Kouadio", "Goglichidze"], "percentuali": [55, 45]},
+      {"posizione": "DC", "giocatori": ["Ziolkowski", "Lucchesi"], "percentuali": [60, 40]},
+      {"posizione": "CC", "giocatori": ["Folorunsho", "Foe Ondoa"], "percentuali": [70, 30]},
+      {"posizione": "CC", "giocatori": ["Mout", "Akinsanmiro"], "percentuali": [50, 50]},
+      {"posizione": "ATT", "giocatori": ["Colpani", "Zeballos"], "percentuali": [70, 30]},
+      {"posizione": "ATT", "giocatori": ["Ngonge", "Cutrone"], "percentuali": [50, 50]}
+    ]
+  },
+  {
+    "squadra": "Napoli",
+    "modulo": "4-3-3",
+    "allenatore": "Allegri",
+    "formazione": "Meret; Di Lorenzo, Rrahmani, Badiashile, Spinazzola; Anguissa/De Bruyne, Lobotka, McTominay; Politano/Vergara, Hojlund, Alisson Santos/Neres/Niang",
+    "ballottaggi": [
+      {"posizione": "CC", "giocatori": ["Anguissa", "De Bruyne"], "percentuali": [55, 45]},
+      {"posizione": "ATT", "giocatori": ["Politano", "Vergara"], "percentuali": [60, 40]},
+      {"posizione": "ATT", "giocatori": ["Alisson Santos", "Neres", "Niang"], "percentuali": [45, 35, 30]}
+    ]
+  },
+  {
+    "squadra": "Parma",
+    "modulo": "4-3-2-1",
+    "allenatore": "Cuesta",
+    "formazione": "Corvi; Delprato, Troilo, Diego Carlos, Valeri; Bernabè, Keita, Fabbian; El Bilal Touré, Lontani; Romero/Elphege",
+    "ballottaggi": [
+      {"posizione": "ATT", "giocatori": ["Romero", "Elphege"], "percentuali": [55, 45]}
+    ]
+  },
+  {
+    "squadra": "Roma",
+    "modulo": "3-4-2-1",
+    "allenatore": "Gasperini",
+    "formazione": "Svilar; Mancini, Ndicka, Hermoso/Balerdi/Koulierakis; Molina/Lulli, Cristante, Konè, Wesley; Mora/Soulè, Dybala; Malen",
+    "ballottaggi": [
+      {"posizione": "DC", "giocatori": ["Hermoso", "Balerdi", "Koulierakis"], "percentuali": [45, 35, 20]},
+      {"posizione": "TD", "giocatori": ["Molina", "Lulli"], "percentuali": [55, 45]},
+      {"posizione": "ATT", "giocatori": ["Mora", "Soulè"], "percentuali": [50, 50]}
+    ]
+  },
+  {
+    "squadra": "Sassuolo",
+    "modulo": "4-3-3",
+    "allenatore": "Aquilani",
+    "formazione": "Muric; Van der Brempt/Cinquegrano, Leysen, Idzes, Doig/Obrador; Bakola, Matic, Adzic/Thorstvedt; Berardi, Bowie/S. Esposito, Lauriente",
+    "ballottaggi": [
+      {"posizione": "TD", "giocatori": ["Van der Brempt", "Cinquegrano"], "percentuali": [65, 35]},
+      {"posizione": "TS", "giocatori": ["Doig", "Obrador"], "percentuali": [60, 40]},
+      {"posizione": "CC", "giocatori": ["Adzic", "Thorstvedt"], "percentuali": [55, 45]},
+      {"posizione": "ATT", "giocatori": ["Bowie", "S. Esposito"], "percentuali": [55, 45]}
+    ]
+  },
+  {
+    "squadra": "Torino",
+    "modulo": "3-4-2-1",
+    "allenatore": "Abate",
+    "formazione": "Perri; Comuzzo, Comert, Coco; Belghali/Fortini, Mandragora/Braganca, Fitz-Jim/Gineitis, Cacciamani; Vlasic, Casadei/Adams; Simeone",
+    "ballottaggi": [
+      {"posizione": "TD", "giocatori": ["Belghali", "Fortini"], "percentuali": [65, 35]},
+      {"posizione": "CC", "giocatori": ["Mandragora", "Braganca"], "percentuali": [60, 40]},
+      {"posizione": "CC", "giocatori": ["Fitz-Jim", "Gineitis"], "percentuali": [65, 35]},
+      {"posizione": "ATT", "giocatori": ["Casadei", "Adams"], "percentuali": [60, 40]}
+    ]
+  },
+  {
+    "squadra": "Udinese",
+    "modulo": "3-5-2",
+    "allenatore": "Runjaic",
+    "formazione": "Okoye; Abankwah/Bertola, Kabasele/Palma, Solet; Vojvoda, Ekkelenkamp, Karlstrom, Piotrowski/Miller/Unai Gomez, Kamara; Zaniolo, Davis",
+    "ballottaggi": [
+      {"posizione": "DC", "giocatori": ["Abankwah", "Bertola"], "percentuali": [60, 40]},
+      {"posizione": "DC", "giocatori": ["Kabasele", "Palma"], "percentuali": [65, 35]},
+      {"posizione": "CC", "giocatori": ["Piotrowski", "Unai Gomez", "Miller"], "percentuali": [50, 30, 30]}
+    ]
+  },
+  {
+    "squadra": "Venezia",
+    "modulo": "3-5-2",
+    "allenatore": null,
+    "formazione": "Stankovic; Bella-Kotchap, Schingtienne, Juan Jesus; Mazzocchi/Hainaut, Basic, Busio, Sohm, Haps/Correia; Adams, Yeboah",
+    "ballottaggi": [
+      {"posizione": "TD", "giocatori": ["Mazzocchi", "Hainaut"], "percentuali": [70, 30], "nota": "Mazzocchi potrebbe agire anche sulla fascia sinistra al posto di Haps"},
+      {"posizione": "TS", "giocatori": ["Haps", "Correia"], "percentuali": [60, 40]}
+    ]
+  }
+];
